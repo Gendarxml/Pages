b9739b6b7ddcc60b6e1b16ddceffcbda58decb3e
Gendarxml <nurinah904@gmail.com>
2024-12-28 05:26:34 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 import { connect } from "cloudflare:sockets";
 
 let isApiReady = false;
-let proxyIP = "47.245.95.160,1443";
+let proxyIP = "47.245.95.160,1443,SG";
 let cachedProxyList = [];
 
 async function getProxyList() {
